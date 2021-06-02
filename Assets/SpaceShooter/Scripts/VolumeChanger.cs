a08f022d502a47d41b0dd7c9e3bf16ac228b7be8
Andrey Ryshkevich <paulbegitson@mail.ru>
2021-06-02 21:20:29 +0300

    Sound fixed

--- Assets/SpaceShooter/Scripts/VolumeChanger.cs
+++ Assets/SpaceShooter/Scripts/VolumeChanger.cs
@@ -8,47 +8,18 @@ namespace SpaceShooter
 {
     public class VolumeChanger : MonoBehaviour
     {
-        private static readonly string firstPlay = "FirstPlay";
         private static readonly string backgroundMusic = "BackgroundMusic";
         private static readonly string soundEffects = "SoundEffects";
 
-        private int firstPlaySave;
-        private float backgroundSound, soundEffectSounds;
         [SerializeField] Slider backgroundSlider, soundeffectsSlider;
         [SerializeField] AudioMixer audioMixer;
-
-        private void Start()
-        {
-            firstPlaySave = PlayerPrefs.GetInt(firstPlay);
-
-            if (firstPlaySave == 0)
-            {
-                backgroundSound = 0.25f;
-                soundEffectSounds = 0.75f;
-                backgroundSlider.value = backgroundSound;
-                soundeffectsSlider.value = soundEffectSounds;
-                PlayerPrefs.SetFloat(backgroundMusic, backgroundSound);
-                PlayerPrefs.SetFloat(soundEffects, soundEffectSounds);
-                PlayerPrefs.SetInt(firstPlay, -1);
-
-            }
-
-            else
-            {
-                backgroundSound = PlayerPrefs.GetFloat(backgroundMusic);
-                backgroundSlider.value = backgroundSound;
-                soundEffectSounds = PlayerPrefs.GetFloat(soundEffects);
-                soundeffectsSlider.value = soundEffectSounds;
-            }
-
-            audioMixer.SetFloat("MusicVolume", backgroundSound);
-            audioMixer.SetFloat("SoundsVolume", soundEffectSounds);
-        }
 
         public void SaveSoundBetweenLevels()
         {
             PlayerPrefs.SetFloat(backgroundMusic, backgroundSlider.value);
             PlayerPrefs.SetFloat(soundEffects, soundeffectsSlider.value);
+
+            PlayerPrefs.Save();
         }
 
         private void OnApplicationFocus(bool focus)
